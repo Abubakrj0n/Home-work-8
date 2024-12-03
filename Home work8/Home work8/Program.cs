31a5ca1d71c856aaee45643ec9e1f4dc9e196c26
Abubakrj0n <big500530@gmail.com>
2024-12-04 04:14:20 +0500

    Add files via upload

---
+++ Home work8/Home work8/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Шумораи бутуни N ворид кунед (> 0):");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N <= 0)
+        {
+            Console.WriteLine("Шумораи N бояд аз 0 зиёд бошад!");
+            return;
+        }
+
+        double factorial = 1;
+        int number = N;
+
+        while (number > 0)
+        {   
+            factorial *= number;
+            number -= 2; 
+        }
+
+        Console.WriteLine($"Дучанд омили {N}!! = {factorial}");
+    }
+}
